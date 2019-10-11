5f0ba8b3ee590e230c626aea8f1641765d34a451
Hasanga Lakdinu <hasangalakdinu@gmail.com>
2019-10-11 16:01:45 +0530

    my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: AuthComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {path : '**', component: AuthComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+Amplify.configure({
+
+  Interactions: {
+    bots: {
+      "WeatherBot": {
+        "name": "WeatherBot",
+        "alias": "$LATEST",
+        "region": "us-east-1",
+      },
+    }
+  }
+});
 
 if (environment.production) {
   enableProdMode();
